f0c77858a407f695fcaab83466223ead9a4ddbe0
cwalz0 <29409656@nebraska.edu>
2026-02-24 22:19:33 -0600

    Complete T1 & T2

--- Tool_Sharers_App/admin.py
+++ Tool_Sharers_App/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, Listing, Transaction, Report, Image, Review
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Transaction)
+admin.site.register(Report)
+admin.site.register(Image)
+admin.site.register(Review)
