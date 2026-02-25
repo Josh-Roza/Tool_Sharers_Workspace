0319fe6c2ccf9da318a0fb2a8222475f2c1b553b
cwalz0 <29409656@nebraska.edu>
2026-02-25 12:10:33 -0600

    Fixed User Bug
    Issue where there were two separate User groups

--- Tool_Sharers_App/models.py
+++ Tool_Sharers_App/models.py
@@ -1,16 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField()
+class User(AbstractUser):
     verified = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     
     def __str__(self):
         return self.username
-
+    
 class Listing(models.Model):
     listing_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
