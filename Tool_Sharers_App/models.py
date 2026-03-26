38d0a6be4cd8f0f6da9507a1664a4703a7d2b834
Josh-Roza <joshroza77@gmail.com>
2026-03-26 02:36:14 +0000

    Created index.html

--- Tool_Sharers_App/models.py
+++ Tool_Sharers_App/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    user_id = models.AutoField(primary_key=True)
     verified = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(unique=True)
     
     def __str__(self):
         return self.username
