1fe9581879d638a24142f1e0f5a28104ff8f4d35
Josh-Roza <joshroza77@gmail.com>
2026-03-27 19:06:25 +0000

    Built Add_user function

--- Tool_Sharers_App/views.py
+++ Tool_Sharers_App/views.py
@@ -8,7 +8,30 @@ def homePage(request):
     return render(request, 'index.html')
 
 def create_user(request):
-    pass
+    success = False
+    added_user = None
+    if request.method == 'POST':
+        form = User_Form(request.POST)
+        if form.is_valid():
+            added_user = form.save()
+            success = True
+            return render(
+                request,
+                "add_user.html",
+                {"form": form,
+                 "success": success,
+                 "added_user": added_user},
+                )
+    else:
+        form = User_Form()
+    return render(
+        request,
+        "add_user.html",
+        {"form": form,
+         "success": success,
+         "added_user": added_user},
+    )
+
 
 def create_listing(request):
     pass
